a986bb6a709d1696da52558f7a392ca8bbe20a59
Kent Huang <kentwelcome@gmail.com>
2021-08-19 11:46:03 +0800

    [Update] one click install url to v0.8.0
    Signed-off-by: Kent Huang <kentwelcome@gmail.com>

--- components/hero.js
+++ components/hero.js
@@ -111,7 +111,7 @@ export default function Hero () {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?stackName=primehub-starter&templateURL=https://primehub.s3.amazonaws.com/cloudformation/primehub-starter-cloudformation.yaml"
+                    href="https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?stackName=primehub-starter&templateURL=https://primehub.s3.amazonaws.com/cloudformation/v0.8.0/primehub-starter-cloudformation.yaml"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     1-Click Install
