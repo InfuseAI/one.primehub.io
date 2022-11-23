89131694617081a5fc77deda129cc1006cb7df8d
Dennis Huang <haicha.tw@gmail.com>
2022-11-23 11:34:11 +0800

    rebuild

---
+++ docs/_next/static/BHeEL1RGv_bcsEAbffpeS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/929-f8df1c5af06dc148.js","static/chunks/pages/index-a00ad778fa413f13.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
